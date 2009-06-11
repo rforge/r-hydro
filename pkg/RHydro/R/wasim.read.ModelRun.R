c6b578a655152e1764d5943be8e841caa72f13c8
dreusser <dreusser@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-06-11 10:22:20 +0000

    - Added possibility to work with multiple runs.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r-hydro@16 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/RHydro/R/wasim.read.ModelRun.R
+++ pkg/RHydro/R/wasim.read.ModelRun.R
@@ -1,27 +1,35 @@
-wasim.read.ModelRun <- function(file, data.types=wasim.data.types){
+wasim.read.ModelRun <- function(files, data.types=wasim.data.types){
    theCall <- paste("wasim.read.ModelRun(", deparse(substitute(file)),")", sep="")
-   pars <- wasim.read.ModelParameters(file)
-   my.ts <- list()
-   for(i in 1:NROW(data.types)){
-       data.types[i,]
-       par.block <- pars[[ paste("[",data.types[i,"par_block"],"]",sep="") ]]
+   fileNr <- 1
+   measuredFluxes <- modelledFluxes <- measuredStates <- modelledStates <- list()
+   for(file in files){
+       pars <- wasim.read.ModelParameters(file)
+       #ToDo convert parameters into usefull HydroWasimParameters
+       my.ts <- list()
+       for(i in 1:NROW(data.types)){
+           data.types[i,]
+           par.block <- pars[[ paste("[",data.types[i,"par_block"],"]",sep="") ]]
 
-       data.file <- par.block[data.types[i,"block_position"]]
-       data.file <- strsplit(data.file, "[ \t]+")[[1]][1] #remove additional stuff at end of the file
-       cat("reading", data.file, "\n")
-       #Datenreihen aufspalten und für RhydroModel-Objekt vorbereiten
-       ts <- wasim.file2HydroTS(file=paste(dirname(file), data.file, sep="/"), data.types[i,"is_recorded"], data.types[i,"is_flux"], units= data.types[i,"units"], type=data.types[i,"type"])
-       if(!is.null(ts)){
-          my.ts <- append(my.ts,ts)
-       } else {
-          data.types <- data.types[,-i]
+           data.file <- par.block[data.types[i,"block_position"]]
+           data.file <- strsplit(data.file, "[ \t]+")[[1]][1] #remove additional stuff at end of the file
+           cat("reading", data.file, "\n")
+           #Datenreihen aufspalten und für RhydroModel-Objekt vorbereiten
+           ts <- wasim.file2HydroTS(file=paste(dirname(file), data.file, sep="/"), data.types[i,"is_recorded"], data.types[i,"is_flux"], units= data.types[i,"units"], type=data.types[i,"type"])
+           if(!is.null(ts)){
+              my.ts <- append(my.ts,ts)
+           } else {
+              data.types <- data.types[,-i]
+           }
        }
+       #ToDo implement all-List entry for shared data
+       #ToDo use data types
+       measuredFluxes[[fileNr]] <- my.ts[data.types[,"is_recorded"] & data.types[,"is_flux"]]
+       modelledFluxes[[fileNr]] <- my.ts[!data.types[,"is_recorded"] & data.types[,"is_flux"]]
+       measuredStates[[fileNr]] <- my.ts[data.types[,"is_recorded"] & !data.types[,"is_flux"]]
+       modelledStates[[fileNr]] <- my.ts[!data.types[,"is_recorded"] & !data.types[,"is_flux"]]
+       fileNr <- fileNr + 1
    }
-   measuredFluxes <- my.ts[data.types[,"is_recorded"] & data.types[,"is_flux"]]
-   modelledFluxes <- my.ts[!data.types[,"is_recorded"] & data.types[,"is_flux"]]
-   measuredStates <- my.ts[data.types[,"is_recorded"] & !data.types[,"is_flux"]]
-   modelledStates <- my.ts[!data.types[,"is_recorded"] & !data.types[,"is_flux"]]
-   toRet <- new("HydroModelRun", parameter=new("HydroWasimParameters", parameters=data.frame(1) ), 
+   toRet <- new("HydroModelRun", parameters=new("HydroWasimParameters", parameters=data.frame(1) ), 
                                 modelledFluxes=modelledFluxes,
 				modelledStates=modelledStates,
 				measuredFluxes=measuredFluxes,
