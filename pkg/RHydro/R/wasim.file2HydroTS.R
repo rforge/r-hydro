ed94107268711824a8573fb2bad6ef64e70347d9
dreusser <dreusser@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-06-18 07:39:19 +0000

    - Support for symbol names in lists included - Header for generated data is implemented now
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r-hydro@22 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/RHydro/R/wasim.file2HydroTS.R
+++ pkg/RHydro/R/wasim.file2HydroTS.R
@@ -1,18 +1,34 @@
-wasim.file2HydroTS <- function(file, is_recorded, is_flux, direction=zoo("out"), units, type){
+wasim.file2HydroTS <- function(file, is_recorded, is_flux,
+direction=zoo("out"), units, type, generated.header.info=NULL){
     data <- wasim.read.table(file)
     if(is.null(data)) return(NULL)
     if(is_recorded){
           coordinate <- wasim.head2spatial(attr(data,"head"))
     } else {
-          #Da noch Position und namen aus Parameterfile
-          coordinate <- SpatialPoints(cbind(4111100,493330))
-          warning("Coordinates for generated data not implemented")
+          if(is.null(generated.header.info) ||
+             NCOL(data) - NCOL(generated.header.info@magnitude)>1 ||
+             NCOL(data) - NCOL(generated.header.info@magnitude)<0){
+             coordinate <- SpatialPoints(data.frame(x=rep(0,NCOL(data)), y=rep(0,NCOL(data))))
+             dimnames(data)[[2]] <- paste("station",1:NCOL(data))
+             
+             warning("No header information for generated data available")
+          } else {
+              if(NCOL(data) - NCOL(generated.header.info@magnitude)==0){
+                   coordinate <- generated.header.info@coordinate
+                   dimnames(data)[[2]] <- dimnames(generated.header.info@magnitude)[[2]]
+              }
+              if(NCOL(data) - NCOL(generated.header.info@magnitude)==1){
+                   coordinate <- SpatialPoints(rbind( generated.header.info@coordinate@coords, c(0,0)))
+                   dimnames(data)[[2]] <- c(dimnames(generated.header.info@magnitude)[[2]], "average")
+              }
+          }
     }
     if(is_flux){
         to.ret <- new("HydroFlux", magnitude = data,
-TSorigin="recorded", coordinate=coordinate, direction=direction, units=units, type=type)
+TSorigin="recorded", coordinate=coordinate, direction=direction,
+units=units, type=type, location.name=dimnames(data)[[2]])
     } else {
-        to.ret <- new("HydroState", magnitude = data, TSorigin="recorded", coordinate=coordinate, units=units, type=type)
+        to.ret <- new("HydroState", magnitude = data, TSorigin="recorded", coordinate=coordinate, units=units, type=type, location.name=dimnames(data)[[2]])
     }
     return(to.ret)
 }
--- pkg/RHydro/R/wasim.read.ModelRun.R
+++ pkg/RHydro/R/wasim.read.ModelRun.R
@@ -1,48 +1,79 @@
-wasim.read.ModelRun <- function(files, data.types=wasim.data.types){
+wasim.read.ModelRun <- function(files){
    theCall <- paste("wasim.read.ModelRun(", deparse(substitute(file)),")", sep="")
    fileNr <- 1
-   measuredFluxes <- modelledFluxes <- measuredStates <-
-          parameters <-  modelledStates <- list()
+   ts.data <- list()
+   for(i in 1:4){
+      ts.data[[i]] <- list()
+      ts.data[[i]]$runs <- list()
+      ts.data[[i]]$shared <- list()
+   }
+   parameters <- new("HydroWasimParameters", parameters=data.frame(1) )
    for(file in files){
        pars <- wasim.read.ModelParameters(file)
        #ToDo convert parameters into usefull HydroWasimParameters
-       parameters[fileNr] <- new("HydroWasimParameters", parameters=data.frame(1) )
+       #Get header information for generated series
+       data.file <- pars[["[routing_model]"]][5]
+       header.ts <- wasim.file2HydroTS(file=paste(dirname(file),
+data.file, sep="/"),
+               is_flux=TRUE,
+               is_recorded=TRUE,
+               units="bla",
+               type="discharge")
+       #Read Data
        my.ts <- list()
        i <- 1
-       while(i <= NROW(data.types)){
-           data.types[i,]
-           par.block <- pars[[ paste("[",data.types[i,"par_block"],"]",sep="") ]]
+       while(i <= NROW(wasim.data.types)){
+           par.block <- pars[[ paste("[",wasim.data.types[i,"par_block"],"]",sep="") ]]
 
-           data.file <- par.block[data.types[i,"block_position"]]
+           data.file <- par.block[wasim.data.types[i,"block_position"]]
            data.file <- strsplit(data.file, "[ \t]+")[[1]][1] #remove additional stuff at end of the file
            cat("reading", data.file, "\n")
            #Datenreihen aufspalten und für RhydroModel-Objekt vorbereiten
-           ts <- wasim.file2HydroTS(file=paste(dirname(file),
-data.file, sep="/"), is_recorded=data.types[i,"is_recorded"], is_flux=data.types[i,"is_flux"], units= data.types[i,"units"], type=data.types[i,"type"])
+           ts <- wasim.file2HydroTS(file=paste(dirname(file), data.file, sep="/"),
+                   is_recorded=wasim.data.types[i,"is_recorded"],
+                   is_flux=wasim.data.types[i,"is_flux"],
+                   units= wasim.data.types[i,"units"], 
+                   generated.header.info=header.ts,
+                   type=wasim.data.types[i,"type"])
            if(!is.null(ts)){
               my.ts <- append(my.ts,ts)
               i <- i+1
            } else {
-              data.types <- data.types[-i,]
+              wasim.data.types <- wasim.data.types[-i,]
            }
        }
        #ToDo implement all-List entry for shared data
-       #ToDo use data types
-       browser()
-       #loop through entries and use [[]] notation to create list with
-       #symbols. check for duplicates and create dummy symbols for
-       #unknown data.types
-       measuredFluxes[[fileNr]] <- my.ts[data.types[,"is_recorded"] & data.types[,"is_flux"]]
-       modelledFluxes[[fileNr]] <- my.ts[(!data.types[,"is_recorded"]) & data.types[,"is_flux"]]
-       measuredStates[[fileNr]] <- my.ts[data.types[,"is_recorded"] & (!data.types[,"is_flux"])]
-       modelledStates[[fileNr]] <- my.ts[(!data.types[,"is_recorded"]) & (!data.types[,"is_flux"])]
+       #Find list entry symbols and sort ts.data according to ts-type
+       for(slot in 1:4){
+           ts.data[[slot]]$runs[[fileNr]] <- list()
+       }
+       for(data.type in (i-1):1){
+          dt <- which(my.ts[[data.type]]@type == data.types$data.type)
+          if(length(dt)==0){
+             warning(paste("unrecognized data type", my.ts[[data.type]]@type, "You may redefine data.types to allow for advanced processing"))
+             if(!exists("unknown")){
+                  unknown <- 1
+             }else{
+                  unknown <- unknown + 1
+             }
+             symbol <- paste("unknwon", unknown, sep="")
+          } else {
+             symbol <- data.types$symbol[dt]
+          }
+          slot <- 1 + wasim.data.types[data.type,"is_recorded"] + 2 * wasim.data.types[data.type,"is_flux"]
+          if(!is.null(ts.data[[slot]][[symbol]])){
+              stop(paste(data.types$data.type[dt], "multiply defined"))
+          }
+          ts.data[[slot]]$runs[[fileNr]][[symbol]] <- my.ts[[data.type]]
+       }
        fileNr <- fileNr + 1
    }
+    slot <- 1 + wasim.data.types[i,"is_recorded"] + 2 * wasim.data.types[i,"is_flux"]
    toRet <- new("HydroModelRun", parameters=parameters, 
-                                modelledFluxes=modelledFluxes,
-				modelledStates=modelledStates,
-				measuredFluxes=measuredFluxes,
-				measuredStates=measuredStates,
+                                modelledFluxes=ts.data[[3]],
+				modelledStates=ts.data[[1]],
+				measuredFluxes=ts.data[[4]],
+				measuredStates=ts.data[[2]],
 				call=theCall)
    return(toRet)
 }
