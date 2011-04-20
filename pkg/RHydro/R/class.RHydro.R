1377a8d86aa5c262352d241bcdceb320f0d56640
wbuytaert <wbuytaert@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-04-20 21:43:07 +0000

    rhydro object layout set
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r-hydro@66 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/RHydro/R/class.RHydro.R
+++ pkg/RHydro/R/class.RHydro.R
@@ -1,9 +1,9 @@
 
 ## This is a class that is very close to HydroRun
 ## but includes everything to run the model, such that generic
-## functions such as simulate() become easy
+## functions such as simulate() become easy to implement
 ## This class is similar to the hydromad object of the eponymous package
-## 
+
 
 setOldClass("xts")
 
@@ -11,16 +11,17 @@ validityRHydro <- function(object) {return()}
 
 setClass("RHydro",
          representation = representation(parameters="HydroModelParameters",
-                                         observations = "list",
-                                         simulations = "list",
+                                         data = "xts",
+                                         simulations = "xts",
                                          metadata = "data.frame",
                                          performance = "data.frame",
                                          supportdata="list",
-                                         model = "character"),
+                                         model = "character",
+                                         options = "list"),
          validity =  validityRHydro,
          prototype = prototype(parameters = new("HydroModelParameters"),
-                               observations = list(),
-                               simulations = list(),
+                               data = xts(),
+                               simulations = xts(),
                                metadata = data.frame(),
                                performance = data.frame(),
                                supportdata = list(),
@@ -35,3 +36,43 @@ setMethod("simulate",
 	    stop("ToDo: Not implemented")
           }
 )
+
+## methods to implement: merge,
+
+setMethod("print",
+    signature(x = "RHydro"),
+    function (x, ...) 
+    {
+      ##cat("Model ID: ",x@parameters@modelID,"\n")
+      cat("\n")
+      cat("Number of model runs: ", nrow(x@parameters@parameters),"\n")
+      cat("Number of parameters: ", ncol(x@parameters@parameters),"\n")
+      cat("Parameter names: ", names(x@parameters),"\n")
+      cat("\n")
+      cat("Data: ", names(x@data) ,"\n")
+      cat("Simulations: ", names(x@simulations),"\n")
+      cat("\n")
+      cat("Calculated performance measures: ", names(x@performance),"\n")
+      cat("Model Support Data: ", names(x@supportdata),"\n")
+      cat("Model: ", x@model, "\n")
+    }
+)
+
+setMethod("summary",
+    signature(object = "RHydro"),
+    function (object, ...) 
+    {
+      print(object, ...)
+    }
+)
+
+setMethod("show",
+    signature(object = "RHydro"),
+    function (object) 
+    {
+      print(object)
+    }
+)
+
+
+
--- pkg/RHydro/R/rhydro.R
+++ pkg/RHydro/R/rhydro.R
@@ -1,9 +1,16 @@
 
 ## construct an rhydro object ready for simulation
 
-rhydro <- function(model, parameters, observations, supportdata){
-  new("RHydro", observations = observations, supportdata = supportdata,
-  model = model)
+rhydro <- function(parameters, data, supportdata = NULL, model,
+                   performance = NULL, ...){
+  new("RHydro",
+      ## todo: make a function that gets the right Hydro*Parameters object here
+      parameters=as("HydroModelParameters",parameters)
+      data = data,
+      supportdata = supportdata,
+      model = model,
+      performance = performance,
+      options = list(...))
 }
 
 
