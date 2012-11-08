5be63a3086f2b64bfc7f3d3ef6649efdac20a6ca
till <till@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-11-08 20:48:02 +0000

    added several hydrological metrics (as announced in E-mail), modified perfindices.R to use optimized functions
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r-hydro@134 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/RHydro/R/hydro_metrics.R
@@ -0,0 +1,233 @@
+#hydrological metrics
+
+runoff_coeff = function (rainfall, runoff) 
+#compute runoff coefficient [-]
+{
+  
+  #rainfall: rainfall in [L]/timestep  
+  #runoff  : runoff   in [L]/timestep  
+  
+  common_length=min(length(rainfall), length(runoff))  
+  
+  return(sum(runoff[1:common_length])/sum(rainfall[1:common_length]))
+}
+
+time_of_conc = function (rainfall, runoff)
+  #compute time of concentration [number of timesteps]
+{
+  #rainfall: rainfall in [L]/timestep  
+  #runoff  : runoff   in [L]/timestep  
+  
+  common_length=min(length(rainfall), length(runoff))  
+  
+  #compute central moments (centre of gravity) for time series
+  cg_rain   =  (1:common_length) %*% rainfall[1:common_length] / sum(rainfall[1:common_length])
+  cg_runoff =  (1:common_length) %*% runoff  [1:common_length] / sum(runoff  [1:common_length])
+  
+  return(as.numeric(cg_runoff-cg_rain))
+}
+
+
+c_smooth = function (rainfall, runoff)
+  #compute smoothing effect of catchment [-]
+{
+  #rainfall: rainfall in [L]/timestep  
+  #runoff  : runoff   in [L]/timestep  
+  
+  common_length=min(length(rainfall), length(runoff))  
+  
+  #compute coefficients of variation for time series
+  cv_rain   =  sd(rainfall[1:common_length]) / mean(rainfall[1:common_length])
+  cv_runoff =  sd(runoff  [1:common_length]) / mean(runoff  [1:common_length])
+  
+  return(cv_runoff/cv_rain)
+}
+
+
+
+#calculate the lower and upper threshold of the flow duration curve
+fdc <- function (x,
+                 lQ.thr=0.66,
+                 hQ.thr=0.33) {
+  quants=quantile(x, probs=c(hQ.thr,lQ.thr)) #get percentiles of runoff
+  return(list("l"=quants[1],"h"=quants[2]))
+}
+
+#slope of flow duration curve for given quantiles
+fdc_slope <- function (runoff,
+                       lQ.thr=0.66,
+                       hQ.thr=0.33, ...) {
+  t= fdc(x=runoff, lQ.thr=lQ.thr, hQ.thr=hQ.thr)
+  return(as.numeric((log(t$h)-log(t$l))/(lQ.thr-hQ.thr))) #as.numeric to strip the name-attribute
+}
+
+
+
+# calculate the Streamflow Elasticity
+sel <- function(rainfall, runoff, agg_dt=1){
+  
+  common_length=min(length(rainfall), length(runoff))  
+  Q=runoff  [1:common_length]  
+  P=rainfall[1:common_length]
+  
+  
+  if (agg_dt!=1) #do temporal aggragation
+  {
+    aggr_index=rep(1:(common_length %/% agg_dt +1), each=agg_dt)[1:common_length]
+    Q=aggregate(x=Q, by=list(aggr_index), FUN=sum)$x
+    P=aggregate(x=P, by=list(aggr_index), FUN=sum)$x
+  } 
+  
+  #dQ=diff(Q)
+  #dP=diff(P)
+  # el = (dQ/mean(Q))  /  (dP/mean(P)) #Sankarasubramaniamet al.(2001), Sawicz et al, 2011: misleading
+  el2 = (Q-mean(Q))/mean(Q)  / ( (P-mean(P))/mean(P)) #Chiew (2006), Fu et al. 2007: makes more sense 
+  
+  return(median(el2[is.finite(el2)]))
+}
+
+bfi = function(runoff, ...)
+{
+  return(sum(baseflow_sep(runoff,...))/sum(runoff))  #return baseflow index or...
+}
+
+baseflow_sep=function(runoff, method="DFM", parms=c(c=0.925, window_size=10, f_lowess=0.1))
+#baseflow separation using various methods, base_flow index
+{
+
+  lr=length(runoff)
+  if (method=="DFM") #baseflow separation; single-pass digital filter method 
+  {
+    qd=array(runoff[1], lr) #direct runoff
+    cc=parms["c"]
+    for(i in 2:lr)
+      qd[i]=max(0, cc*qd[i-1]+(1+cc)/2*(runoff[i]-runoff[i-1]))
+    qb=runoff-qd
+  }    
+
+  if (method=="constant_slope") #baseflow separation; constant slope (Dingman 2002, p. 375) 
+  {
+    min_points=which(diff(sign(diff(runoff)))>0)+1 #find minimum points in time series
+    #points(min_points, runoff[min_points])
+    slope=parms["c_slope"]
+    if (is.na(slope))
+      slope=diff(range(runoff))/lr/2
+    qb=runoff
+    while(length(min_points)>0)
+    {
+      i=min_points[1]
+      qb[i:lr]=runoff[i]+(0:(lr-i))*slope  #draw constant slope line
+      tt=which(runoff < qb) #search for intersection with hydrograph
+      if (any(tt))
+      {
+        intersect_i=min(tt)      #search for intersection with hydrograph
+        qb[intersect_i:lr]=runoff[intersect_i:lr]
+        min_points=min_points[min_points>=intersect_i]
+      } else break
+    }
+    #lines(qb, col="blue")
+  }    
+  
+  if (method=="RLSWM") #baseflow separation; rescaled lowess-smoothed window minima 
+  {
+    qb=array(0, lr)     #create array for base flow data
+  
+    window_size=parms["window_size"]  #time window to be used to derive baseflow
+    f_lowess=parms["f_lowess"]
+    
+    for (i in 1:length(runoff))
+    {
+      recent_q=runoff[max(1,i-window_size):i]  #determine minimum in window
+      qb[i]=min(recent_q)  
+    }
+    base_flow=lowess(qb,f=f_lowess)$y  #smooth minima
+    
+    ratio=qb/runoff 
+    if (max(ratio)>1)
+      qb=qb/max(ratio)   #rescale baseflow to ensure that baseflow is alway less than total flow
+  }
+
+  return(qb) #base flow time series
+}
+
+#rising limb density
+rising_limb_density <- function(runoff){
+  rising=diff(runoff)>0
+  n_rl= sum(diff(which(rising))>1) #number of rising limbs
+  t_r = sum(rising)                #number of timsteps with rising limbs
+  return(n_rl/t_r)
+}
+
+
+hpc <- function(runoff){ #high pulse count (Clausen & Biggs, 2000 (FRE3) in Yadar et al 2007)
+  
+  
+  high=runoff > 3*median(runoff)
+  n_high= sum(diff(which(high))>1) #number high pulse events
+
+  return(n_high/length(runoff)) 
+}
+
+
+q10 <- function(runoff){ #Flow exceeded 10% of the time divided by Q50 (Clausen & Biggs, 2000 )
+  quants=quantile(x=runoff, probs=c(0.5,0.9))
+  return(as.numeric(quants[2]/quants[1]))
+}
+
+
+
+fft_fit <- function (runoff, rainfall, doplot=FALSE) {
+  #fit two-segmented linear regression into log-log-plot of FFT of time series
+  
+  library(segmented)
+  y_p=fft(scale(rainfall))
+  y_q=fft(scale(runoff))
+  
+  len=length(y_p)
+  freqs=(1:len)/len
+  #  plot(log(freqs[1:(len/2)]),log(abs(y2[1:(len/2)])),type="l")
+  
+  
+  dati<-data.frame(x=log(freqs[1:(len/2)]), y_p=log(abs(y_p[1:(len/2)])), y_q=log(abs(y_q[1:(len/2)])))
+  #resample log-log plot to (coarser) equidistant resolution
+  resamp_points=50
+  dati$resamp=(ceiling( (dati$x-min(dati$x))/diff(range(dati$x))*resamp_points))
+  #dati$resamp[1]=1 #assign first frequency to first bin, too
+  resampled=aggregate(x=dati[-1,],by=list(bin=dati$resamp[-1]),FUN=mean)
+  
+  
+  #find breakpoints
+  lm_p<-lm(y_p~x,data=resampled)
+  lm_q<-lm(y_q~x,data=resampled)
+  
+  o_p<-segmented(lm_p,seg.Z=~x,psi=list(x=mean(resampled$x)),
+                 control=seg.control(display=FALSE))
+  o_q<-segmented(lm_q,seg.Z=~x,psi=list(x=mean(resampled$x)),
+                 control=seg.control(display=FALSE))
+  
+  if (doplot)
+  {
+    plot(dati$x[-1], dati$y_p[-1],type="l", xlab="log(freq)", ylab="power")
+    lines(dati$x,dati$y_q,type="l", col="grey")
+    
+    points(resampled$x, resampled$y_p, col="red")
+    points(resampled$x, resampled$y_q, col="blue")
+    
+    lines(resampled$x,predict(o_p), col="red")
+    lines(resampled$x,predict(o_q), col="blue")
+  }
+  return(cbind(p=c(slopes_p=slope(o_p)$x[,"Est."], breakp=o_p$psi[,"Est."]),
+               q=c(slopes_p=slope(o_q)$x[,"Est."], breakp=o_q$psi[,"Est."])))
+}
+
+
+filter_ratio=function(runoff, rainfall, doplot=FALSE) #ratio of frequency recession constants
+{
+  rr=fft_fit(runoff=runoff, rainfall=rainfall, doplot=doplot)
+  return(filter_ratio=rr[2,"p"]/rr[2,"q"])
+}
+
+
+
+
+
--- pkg/RHydro/R/perfindices.R
+++ pkg/RHydro/R/perfindices.R
@@ -1,104 +1,4 @@
-# This function calculates the lower and upper threshold of the flow duration curve
-# code obtained modifying fdc.R from hydroTSM package
-fdc <-function(x, ...) UseMethod("fdc")
-
-fdc.default <- function (x,
-                         lQ.thr=0.66,
-                         hQ.thr=0.33,
-                         log="y") {
-  
-  # Returns the position in the vector 'x' where the scalar 'Q' is located
-  Qposition <- function(x, Q) {
-    Q.dist  <- abs(x - Q)
-    Q.index <- which.min( Q.dist )
-    return(Q.index)
-  } # end
-  
-  if (log == "y") {
-    x.zero.index <- which(x==0)
-    if (length(x.zero.index) > 0 ) {
-      x <- x[-x.zero.index]
-    } # IF end
-  } # IF end
-  
-  x <- as.numeric(x)
-  
-  # Storing the original values
-  x.old <- x
-  
-  # 1) Sort 'x' in drecreasing order. This is just for avoiding misleading
-  #lines when using 'type="o"' for plotting
-  x <- sort(x)
-  
-  # Index with the position of the original values
-  ind <- match(x.old, x)
-  
-  # 2) Compute the length of 'x'
-  n <- length(x)
-  
-  # 3) Creation of the output vector
-  dc <- rep(NA, n)
-  
-  # 4) Exceedence Probability
-  dc[1:n] <- sapply(1:n, function(j,y) {
-    dc[j] <- length( which(y >= y[j]) )
-  }, y = x)
-  
-  # Computing the probabilitites
-  dc <- dc/n
-  
-  # Finding the flow values corresponding to the 'lQ.thr' and 'hQ.thr' pbb of excedence
-  x.lQ <- x[Qposition(dc, lQ.thr)]
-  x.hQ <- x[Qposition(dc, hQ.thr)]
-  
-  # Restoring the original positions
-  dc <- dc[ind]
-  
-  qthresholds <- list("l"=x.lQ,"h"=x.hQ)
-  
-  return(qthresholds)
-  
-}
-
-# this function calculates the Streamflow Elasticity
-sel <- function(P,Q){
-    
-  l <- length(P)
-  
-  el <- rep(NA,l-1)
-  
-  for (i in 2:l) {
-    dQ <- Q[i]-Q[i-1]
-    dP <- P[i]-P[i-1]
-    
-    if (is.finite(dQ*Q[i]/dP/P[i])) el[i-1] <- dQ*Q[i]/dP/P[i]
-  } 
-  
-  el1 <- el[which(el!="NA")] 
-  results <- median(el1)
-  
-  return(results)
-}
-
-# This function calculates the High Pulse Count
-diffhpc <- function(Qo,Qs){
-  
-  m <- 3*median(Qo)
-  l <- length(Qo)
-  
-  count_o <- count_s <- 0
-  
-  for (i in 1:l) {
-    if (Qo[i] > m) count_o <- count_o + 1
-    if (is.na(Qs[i])==FALSE && Qs[i]>m) count_s <- count_s + 1
-  }
-  
-  result <- count_o - count_s
-  return(result)
-}
-
-
-# This function calculates 10 performance indices
+# This function calculates 10 performance and streamflow indices
 # inputs:
 # Po = observed precipitation [mm/timestep]
 # Qo = observed streamflow    [mm/timestep]
@@ -117,9 +17,9 @@ perfindices <- function(Po,Qo,Qs,Qb){
   #prmse  <- RMSE(Qo,Qs)                                          # Root mean square error (from qualV) - it should tend to zero
   rrcoeff <- sum(Qs)/sum(Po)                                     # Rainfall-Runoff coefficient                                            
   bfindex <- sum(Qb)/sum(Qs)                                     # Baseflow Index
-  slopfdc <- (log(t$h)-log(t$l))/(0.66 - 0.33)                   # Slope of flow duration curve
+  slopfdc <- fdc_slope(Qs, 0.33, 0.66)                               # Slope of flow duration curve
   stelast <- sel(Po,Qs)                                          # Streamflow Elasticity
-  diffhpc <- diffhpc(Qo,Qs)                                      # Difference of High Pulse Count (observed - simulated) - it should tend to zero
+  diffhpc <- hpc(Qo)-hpc(Qs)                                      # Difference of High Pulse Count (observed - simulated) - it should tend to zero
   
   return( list("lagtime"=lagtime,
                "meanerr"=meanerr,
