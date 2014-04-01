d5492cb1cf0f8fd9619799ef196dea6bd4206084
till <till@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-04-01 08:15:36 +0000

    - added function for applying unit hydrograph to given rainfall series, made visible publicly - fixed minor inaccuracy in UHG - minor updates for documentation
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r-hydro@170 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/RHydro/R/unit.hydrograph.storage.cascade.R
+++ pkg/RHydro/R/unit.hydrograph.storage.cascade.R
@@ -2,13 +2,13 @@
 unit.hydrograph.storage.cascade = function(n,k,delta_t)
 {
   #determine required length of unit hydrograph to capture at least 99% of response
-  h=array(0,100)
+  h=array(0,100) #initial estimate on length of h
   for (i in 1:100)
   {
-    j = 1:100 + (i-1)*100
-    h[1:100 + (i-1)*100] = ((j-0.5)*delta_t)^(n-1)/ (k^n*gamma(n))*exp(-(j-0.5)*delta_t/k)
+    j = 0:99 + (i-1)*100 #compute next 100 ordinates
+    h  [j+1] = (j*delta_t)^(n-1)/ (k^n*gamma(n))*exp(-j*delta_t/k)
 
-    cutoff=min(c(Inf,which(h/cumsum(h)<0.01))) #cutoff UH after 99% of volume
+    cutoff=min(c(Inf,which(h/cumsum(h)<0.001))) #cutoff UH after 99% of volume
 #    cutoff=min(c(Inf,which(cumsum(h)>0.99))) #cutoff UH after 99% of volume
     if (!is.finite(cutoff))
       next else
@@ -21,8 +21,33 @@ unit.hydrograph.storage.cascade = function(n,k,delta_t)
   if (!is.finite(cutoff))
   {
    h=NULL
-   #stop("Parameters of storage cascade result in too long unit hydrograph.")
+   stop("Parameters of storage cascade result in too long unit hydrograph. Increase delta_t.")
   }
   return(h)
 }
 
+#apply (multiple) unit hydrographs to rainfall series
+apply_uhg=function(rainfall, uhg_params, delta_t)
+{
+  if (length(uhg_params) == 2) uhg_params = matrix(c(uhg_params, a=1), ncol=3, dimnames=list(NULL,c("n","k","a")))
+  
+  if (length(uhg_params) %% 3 != 0) stop("uhg_params must be an two-element named vector or named three-column matrix")
+  
+  if (sum(uhg_params[,"a"]) != 1) stop("sum of uhg_params[,'a'] must be 1")
+  
+  n_uhg = nrow(uhg_params) #number of cascades (UHGs) to treat
+  
+  hydrograph=NULL
+  for (i in 1:n_uhg)
+  {
+    cur_uhg = unit.hydrograph.storage.cascade(n=uhg_params[i,"n"], k=uhg_params[i,"k"], delta_t = delta_t)
+    
+    partial_hydrograph = convolve(rainfall, rev(cur_uhg), type = "o") #construct partial hydrograph
+    
+    if(length(partial_hydrograph) > length(hydrograph)) #prolong hydrograph vector, if necessary
+      hydrograph=c(hydrograph, rep(0, length(partial_hydrograph) - length(hydrograph)))
+    hydrograph[1:length(partial_hydrograph)] = hydrograph[1:length(partial_hydrograph)] + uhg_params[i,"a"] * partial_hydrograph  #add weighted fraction of this unit hydrograph to total
+    
+  }
+  return(hydrograph)
+}
